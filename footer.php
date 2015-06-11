44baaa09b38454b6d7bfc324f72b2f7e14088cca
Nícholas André <nicholas@iotecnologia.com.br>
2015-06-11 13:54:30 +0000

    Removing  scripts

--- footer.php
+++ footer.php
@@ -6,19 +6,13 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; Your Website <?php date('Y'); ?></p>
                 </div>
             </div>
         </footer>
 
     </div>
     <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
 
     <?php wp_footer(); ?>
 
